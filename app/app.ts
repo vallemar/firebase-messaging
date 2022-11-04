a56a83dc9a362c414bcf4dab5ef9f0ba0969f497
Juan de Dios Martinez Vallejo <vallemardeveloper@gmail.com>
2022-11-04 17:13:50 +0100

    Init commit

--- app/app.ts
+++ app/app.ts
@@ -4,8 +4,49 @@ import Home from './components/Home.vue'
 declare let __DEV__: boolean;
 
 // Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = !__DEV__
+Vue.config.silent = true
 
 new Vue({
   render: (h) => h('frame', [h(Home)]),
 }).$start()
+
+
+
+console.log("INIT APP")
+import {firebase} from '@nativescript/firebase-core'
+import '@nativescript/firebase-messaging'
+import {AuthorizationStatus} from "@nativescript/firebase-messaging"; // only needs to be imported 1x
+
+
+async function requestUserPermission() {
+  const authStatus = await firebase()
+    .messaging()
+    .requestPermission({
+      android: {
+        alert: true
+      }
+    })
+
+  const enabled =
+    authStatus === AuthorizationStatus.AUTHORIZED ||
+    authStatus === AuthorizationStatus.PROVISIONAL
+  console.log(enabled)
+  if (enabled) {
+    console.log('Authorization status:', authStatus)
+
+    const didRegister = await firebase().messaging().registerDeviceForRemoteMessages()
+    console.log(didRegister)
+    console.log(await firebase()
+      .messaging().getToken())
+  }
+}
+
+requestUserPermission();
+
+firebase()
+  .messaging()
+  .onMessage(async (remoteMessage) => {
+    console.log("onMessage MESSAGEEE!!!!!!")
+  });
+
+
